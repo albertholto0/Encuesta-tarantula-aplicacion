7a0a2ce60d4bbee8214e9ebf0aaba092919f66ee
Albert Contreras <albertmendoza019@gmail.com>
2025-06-27 09:28:56 -0600

    .

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,8 +4,8 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   // Ahora es pública (sin guion bajo)
-  static const String baseUrl = 'http://10.0.2.2/api';
-  // static const String baseUrl = 'http://192.168.137.215/api';
+  // static const String baseUrl = 'http://10.0.2.2/api';
+  static const String baseUrl = 'http://192.168.137.215/api';
 
   // Método para obtener la URL de visualización
   static String getVisualizationUrl() {
